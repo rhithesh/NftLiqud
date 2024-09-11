d05dc0ed6d32c20651b52547f1b87f3f3340b825
iharsh02 <ht266005@gmail.com>
2024-09-12 00:06:30 +0530

    Fix the responsiveness of the swap page

--- components/NftComponent/NftCollectionDisplay.tsx
+++ components/NftComponent/NftCollectionDisplay.tsx
@@ -1,11 +1,11 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import  Card  from "@/components/NftComponent/NftCard";
+import Card from "@/components/NftComponent/NftCard";
 import { getAssetsByOwner } from "@/helperFunctions/getAssetsByOwner";
 import { useWallet } from "@solana/wallet-adapter-react";
 
 interface CardProps {
-  id : string;
+  id: string;
   name: string;
   imageurl: string;
   description: string;
@@ -13,14 +13,12 @@ interface CardProps {
 
 export function NftDisplay() {
   const [nftCollection, setNftCollection] = useState<CardProps[]>([]);
-
   const { publicKey } = useWallet();
 
   useEffect(() => {
     if (!publicKey) {
       return;
     }
-
     const fetchNft = async () => {
       try {
         const assets = await getAssetsByOwner(publicKey.toString());
@@ -29,15 +27,18 @@ export function NftDisplay() {
         console.log("Error connecting to wallet:", error);
       }
     };
-
     fetchNft();
   }, [publicKey]);
 
   return (
-    <div className="grid grid-cols-4 gap-4 p-6"> {/* Added padding */}
-      {nftCollection.map((card: CardProps, index) => (
-        <Card key={index} data={card} />
-      ))}
+    <div className="flex justify-center items-center p-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 w-full max-w-7xl">
+        {nftCollection.map((card: CardProps, index) => (
+          <div key={index} className="flex justify-center">
+            <Card data={card} />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
